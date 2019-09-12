3d74d9b6d1e905e98322bbd69d59534ef0d67542
reitmas325 <42395690+reitmas325@users.noreply.github.com>
2019-09-12 16:56:29 -0500

    Driver Program

--- main.c
+++ main.c
@@ -2,8 +2,10 @@
 
 int main(int argc, char const *argv[])
 {
-    StringPtr palabra = new_String("Hola\n");
+    StringPtr palabra = new_String("Hola");
+    Str_toUpper(palabra);
     printf("%s\n", Str_get(palabra));
 
+    
     return 0;
 }
